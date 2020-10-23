73fd55fa0a8a92814b519d797da739d2d9874a93
Mani <realmanisingh@gmail.com>
2020-10-23 13:43:00 -0400

    Created functions for scraping articles and then saving the articles to a CSV file

--- web_scraper.py
+++ web_scraper.py
@@ -5,11 +5,11 @@ import pandas as pd
 import re
 import os
 
-def remove_html_tags(text):
+def remove_html_tags(text: str) -> str:
     """Remove html tags from a string"""
-    import re
     clean = re.compile('<.*?>')
     return re.sub(clean, '', str(text))
+
 
 def scrape_article_links(year: int) -> List[str]:
     """
@@ -20,24 +20,55 @@ def scrape_article_links(year: int) -> List[str]:
     # Take into considerations leap years and days when no articles are published
     pass
 
-def scrape_articles(urls: List[str], year:int) -> List[str]:
+
+def scrape_articles(urls: List[str]) -> List[str]:
     """
     Scrape contents of articles given the URLs
     param urls: list of urls
     param year: the year of the articles that will be scraped
     return: a list of articles
     """
-    pass
+    base_url = "https://www.wbur.org"
+    articles = []
+    
+    for url in urls:
+        url = f'{base_url}{url}'
+        req = Request(url)
+        html_page = urlopen(req)
+        
+        soup = BeautifulSoup(html_page, 'lxml')
+        paragraphs = soup.find_all('p', class_="")
+        
+        # Removing HTML tags from text
+        clean_text = []
+        for paragraph in paragraphs:
+            clean_text.append(remove_html_tags(paragraph))
+            
+        clean_str = ''.join(clean_text)
+        
+        articles.append(clean_str)
+        
+    return articles
 
-def create_csv(articles: List[str]) -> None:
+
+def create_csv(articles: List[str], year: int) -> None:
     """
     Creates and saves a csv file containing a 
     single column for the scraped articles
     param articles: a list of articles
     return: None, saves the csv to disk
     """
-    pass
+    df = pd.DataFrame({
+        'text': articles
+    })
+    
+    df.to_csv(f'wbur{year}.csv')
 
+
+test_scrape = scrape_articles(["/news/2014/01/01/family-homelessness", "/news/2014/01/01/james-avery", "/news/2014/01/01/woman-killed-in-boston-drawbridge-accident"])
+test_csv = create_csv(test_scrape, 2014)
+
+'''
 # Opening the archive page that contains links to the articles that were published on 01/01/2014
 req = Request("https://www.wbur.org/news/archive/2014/01/01")
 html_page = urlopen(req)
@@ -65,7 +96,7 @@ html_page = urlopen(req)
 soup = BeautifulSoup(html_page, "lxml")
 
 # Viewing the html
-print(soup.prettify())
+#print(soup.prettify())
 
 # Saving all the paragraphs in a list
 paragraphs = soup.find_all('p', class_="")
@@ -90,3 +121,4 @@ f = open("avery.txt", 'w')
 for i in clean:
     f.write(i)
 f.close()
+'''
